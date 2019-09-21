89d213582c8f3c5fd4f64482af82756bf6a72a3c
Andres Cubillos <mincubill@gmail.com>
2019-09-21 00:55:14 -0300

    Cliente inicial con MVC

---
+++ Cliente/SigloXXI/SigloXXI/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SigloXXI.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Display(Name = "Nombre de usuario: ")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Display(Name = "Contraseña: ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public int AccessLevel { get; set; }
+
+    }
+}
