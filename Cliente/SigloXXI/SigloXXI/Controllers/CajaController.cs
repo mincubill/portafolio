78aa1ead93a7b2d988897063f22f3e9f09eb2999
andres cubillos <mincubill@gmail.com>
2019-09-27 22:08:33 -0300

    Agregadas clases para los demas mantenedores

---
+++ Cliente/SigloXXI/SigloXXI/Controllers/CajaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SigloXXI.Controllers
+{
+    public class CajaController : Controller
+    {
+        // GET: Caja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cliente/SigloXXI/SigloXXI/Controllers/MesasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SigloXXI.Controllers
+{
+    public class MesasController : Controller
+    {
+        // GET: Mesas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
