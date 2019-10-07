27a7ad583def0ec4bb704d7938c0f032ae5585f9
Rigo <ri.manquel@alumnos.duoc.cl>
2019-10-07 13:23:13 -0400

    Mantenedor de mesas, falta crear vista

--- Cliente/SigloXXI/SigloXXI.Data/Mesas.cs
+++ Cliente/SigloXXI/SigloXXI.Data/Mesas.cs
@@ -8,5 +8,69 @@ namespace SigloXXI.Data
 {
     class Mesas
     {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public int Capacidad { get; set; }
+        public string Url { get; set; }
+        public Mesas()
+        {
+            Url = "http://weasdf.ddns.net:8082";
+        }
+
+        public bool CrearMesa(Mesas mesa)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Users>.Url = Url;
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", mesa.Id.ToString() },
+                { "numero",  mesa.Numero.ToString() },
+                {"capacidad",  mesa.Capacidad.ToString() },
+            };
+            return JsonHelper<Productos>.Post(queryParams, "/mesas/crear-mesa");
+        }
+
+        public bool ActualizarMesa(Mesas mesa)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Users>.Url = Url;
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", mesa.Id.ToString() },
+                { "numero",  mesa.Numero.ToString() },
+                {"capacidad",  mesa.Capacidad.ToString() },
+            };
+            return JsonHelper<Users>.Put(queryParams, "/mesas/actualizar-mesa/" + mesa.Id);
+        }
+
+        public List<Mesas> ObtenerMesas()
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Mesas>.Url = Url;
+            var result = JsonHelper<Mesas>.GetList("/mesas/obtener-mesas");
+            return result;
+        }
+
+        public Mesas ObtenerMesa(int id)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Mesas>.Url = Url;
+            var queryParams = new Dictionary<string, string>();
+            var res = JsonHelper<Mesas>.Get(queryParams, "/mesas/buscar-mesa/" + id.ToString());
+            return res;
+        }
+
+        public bool EliminarMesa(int id)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Mesas>.Url = Url;
+            var queryParams = new Dictionary<string, string>();
+            return JsonHelper<Mesas>.Delete(queryParams, "/mesas/eliminar-mesa/" + id.ToString());
+        }
     }
 }
---
+++ Cliente/SigloXXI/SigloXXI/Controllers/MesaController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SigloXXI.Data;
+using SigloXXI.Models;
+using System.Web.Mvc;
+
+
+namespace SigloXXI.Controllers
+{
+    public class MesaController : Controllers
+    {
+        public ActionResult VerMesas()
+        {
+            var mesa = new Mesas();
+            ViewData["Mesas"] = mesa.ObtenerMesas();
+            return View();
+        }
+
+        [HttpGet]
+        public actionResult AgregarMesas()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarMesas(MesaModel model)
+        {
+            var mesa = new Mesas()
+            {
+                Id = model.Id,
+                Numero = model.Numero,
+                Capacidad = model.Capacidad,
+            };
+            mesa.CrearMesa(mesa);
+            return RedirectToAction("VerMesas");
+        }
+
+        [HttpGet]
+        public ActionResult EditarMesas(int id)
+        {
+            var mesa = new Mesas();
+            mesa = mesa.ObtenerMesa(id);
+            MesaModel model = new MesaModel
+            {
+                Id = mesa.Id,
+                Numero = mesa.Numero,
+                Capacidad = mesa.Capacidad,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditarMesas(MesaModel model)
+        {
+            var mesa = new Mesas()
+            {
+                Id = model.Id,
+                Numero = model.Numero,
+                Capacidad = model.Capacidad,
+            };
+            mesa.ActualizarMesa(mesa);
+            return RedirectToAction("VerMesas");
+        }
+
+        public ActionResult EliminarMesas(int id)
+        {
+            var mesa = new Mesas();
+            mesa.EliminarMesa(id);
+            return RedirectToAction("VerMesas");
+        }
+    }
+}
