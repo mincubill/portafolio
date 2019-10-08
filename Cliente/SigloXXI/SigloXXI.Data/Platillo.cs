17730ea3c605c74e90f76c18d6bdd08e05df0cfe
Rigo <ri.manquel@alumnos.duoc.cl>
2019-10-08 07:45:43 -0400

    Mantenedor de platillos, falta la vista

--- Cliente/SigloXXI/SigloXXI.Data/Platillo.cs
+++ Cliente/SigloXXI/SigloXXI.Data/Platillo.cs
@@ -8,5 +8,71 @@ namespace SigloXXI.Data
 {
     class Platillo
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Tiempo { get; set; }
+        public string Url { get; set; }
+
+        public Platillo()
+        {
+            Url = "http://weasdf.ddns.net:8082";
+        }
+
+        public bool CrearPlatillo(Platillo platillo)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Users>.Url = Url;
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", platillo.Id.ToString() },
+                {"nombre",  platillo.Nombre},
+                {"tiempo",  platillo.Tiempo.ToString()},
+
+            };
+            return JsonHelper<Productos>.Post(queryParams, "/platillos/crear-platillo");
+        }
+
+        public bool ActualizarPlatillo(Platillo platillo)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Users>.Url = Url;
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", platillo.Id.ToString() },
+                {"nombre",  platillo.Nombre},
+                {"tiempo",  platillo.Tiempo.ToString()},
+            };
+            return JsonHelper<Users>.Put(queryParams, "/productos/actualizar-producto/" + platillo.Id);
+        }
+
+        public List<Platillo> ObtenerPlatillos()
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Platillo>.Url = Url;
+            var result = JsonHelper<Platillo>.GetList("/platillos/obtener-platillo");
+            return result;
+        }
+
+        public Platillo ObtenerPlatillo(int id)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Platillo>.Url = Url;
+            var queryParams = new Dictionary<string, string>();
+            var res = JsonHelper<Platillo>.Get(queryParams, "/platillos/buscar-platillo/" + id.ToString());
+            return res;
+        }
+
+        public bool EliminarPlatillo(int id)
+        {
+            var url = new UriBuilder(Url);
+            ConexionHelper.Cliente.BaseAddress = new Uri(Url);
+            JsonHelper<Platillo>.Url = Url;
+            var queryParams = new Dictionary<string, string>();
+            return JsonHelper<Platillo>.Delete(queryParams, "/platillos/eliminar-platillos/" + id.ToString());
+        }
     }
 }
--- Cliente/SigloXXI/SigloXXI/Controllers/PlatilloController.cs
+++ Cliente/SigloXXI/SigloXXI/Controllers/PlatilloController.cs
@@ -2,16 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SigloXXI.Data;
+using SigloXXI.Models;
 using System.Web.Mvc;
 
 namespace SigloXXI.Controllers
 {
     public class PlatilloController : Controller
     {
-        // GET: Platillo
-        public ActionResult Index()
+        public ActionResult VerPlatillos()
+        {
+            var plat = new Platillo();
+            ViewData["Platillo"] = plat.ObtenerPlatillos();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AgregarPlatillo()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarPlatillo(PlatilloModel model)
+        {
+            var plat = new Platillo()
+            {
+                Id = model.Id,
+                Nombre = model.Nombre,
+                Tiempo = model.Tiempo,
+            };
+            plat.CrearPlatillo(plat);
+            return RedirectToAction("VerPlatillos");
+        }
+
+        [HttpGet]
+        public ActionResult EditarPlatillo(int id)
+        {
+            var plat = new Platillo();
+            plat = plat.ObtenerPlatillo(id);
+            PlatilloModel model = new PlatilloModel
+            {
+                Id = plat.Id,
+                Nombre = plat.Nombre,
+                Tiempo = plat.Tiempo,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditarPlatillo(PlatilloModel model)
+        {
+            var plat = new Productos()
+            {
+                Id = model.Id,
+                Nombre = model.Nombre,
+                Tiempo = model.Tiempo,
+            };
+            plat.ActualizarPlatillo(plat);
+            return RedirectToAction("VerPlatillos");
+        }
+
+        public ActionResult EliminarPlatillo(int id)
+        {
+            var plat = new Platillo();
+            plat.EliminarPlatillo(id);
+            return RedirectToAction("VerPlatillos");
         }
     }
 }
