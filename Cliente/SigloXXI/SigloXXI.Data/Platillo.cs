ef5ae0e0afc18fecb20dfdb54270110dad51d8c6
andres cubillos <mincubill@gmail.com>
2019-10-08 20:29:02 -0300

    Merge con rc_platillos

--- Cliente/SigloXXI/SigloXXI.Data/Platillo.cs
+++ Cliente/SigloXXI/SigloXXI.Data/Platillo.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace SigloXXI.Data
 {
-    class Platillo
+    public class Platillo
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Tiempo { get; set; }
+        public string Token { get; set; }
+
+        public bool CrearPlatillo(Platillo platillo)
+        {
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", platillo.Id.ToString() },
+                {"nombre",  platillo.Nombre},
+                {"tiempo",  platillo.Tiempo.ToString()},
+
+            };
+            JsonHelper<Platillo>.Token = this.Token;
+            return JsonHelper<Platillo>.Post(queryParams, "/platillos/crear-platillo");
+        }
+
+        public bool ActualizarPlatillo(Platillo platillo)
+        {
+            
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", platillo.Id.ToString() },
+                {"nombre",  platillo.Nombre},
+                {"tiempo",  platillo.Tiempo.ToString()},
+            };
+            JsonHelper<Platillo>.Token = this.Token;
+            return JsonHelper<Platillo>.Put(queryParams, "/productos/actualizar-producto/" + platillo.Id);
+        }
+
+        public List<Platillo> ObtenerPlatillos()
+        {
+            JsonHelper<Platillo>.Token = this.Token;
+            var result = JsonHelper<Platillo>.GetList("/platillos/obtener-platillo");
+            return result;
+        }
+
+        public Platillo ObtenerPlatillo(int id)
+        {
+            JsonHelper<Platillo>.Token = this.Token;
+            var queryParams = new Dictionary<string, string>();
+            var res = JsonHelper<Platillo>.Get(queryParams, "/platillos/buscar-platillo/" + id.ToString());
+            return res;
+        }
+
+        public bool EliminarPlatillo(int id)
+        {
+            JsonHelper<Platillo>.Token = this.Token;
+            var queryParams = new Dictionary<string, string>();
+            return JsonHelper<Platillo>.Delete(queryParams, "/platillos/eliminar-platillos/" + id.ToString());
+        }
     }
 }
--- Cliente/SigloXXI/SigloXXI/Controllers/PlatilloController.cs
+++ Cliente/SigloXXI/SigloXXI/Controllers/PlatilloController.cs
@@ -2,16 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SigloXXI.Data;
+using SigloXXI.Models;
 using System.Web.Mvc;
 
 namespace SigloXXI.Controllers
 {
     public class PlatilloController : Controller
     {
-        // GET: Platillo
-        public ActionResult Index()
+        string _token;
+        public ActionResult VerPlatillos()
         {
+            _token = Session["Token"].ToString();
+            var plat = new Platillo() { Token = _token };
+            ViewData["Platillo"] = plat.ObtenerPlatillos();
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult AgregarPlatillo()
+        {
+            _token = Session["Token"].ToString();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarPlatillo(PlatilloModel model)
+        {
+            _token = Session["Token"].ToString();
+            var plat = new Platillo()
+            {
+                Token = _token,
+                Id = model.Id,
+                Nombre = model.Nombre,
+                Tiempo = model.Tiempo,
+            };
+            plat.CrearPlatillo(plat);
+            return RedirectToAction("VerPlatillos");
+        }
+
+        [HttpGet]
+        public ActionResult EditarPlatillo(int id)
+        {
+            _token = Session["Token"].ToString();
+            var plat = new Platillo() { Token = _token };
+            plat = plat.ObtenerPlatillo(id);
+            PlatilloModel model = new PlatilloModel
+            {
+                Id = plat.Id,
+                Nombre = plat.Nombre,
+                Tiempo = plat.Tiempo,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditarPlatillo(PlatilloModel model)
+        {
+            _token = Session["Token"].ToString();
+            var plat = new Platillo()
+            {
+                Token = _token,
+                Id = model.Id,
+                Nombre = model.Nombre,
+                Tiempo = model.Tiempo,
+            };
+            plat.ActualizarPlatillo(plat);
+            return RedirectToAction("VerPlatillos");
+        }
+
+        public ActionResult EliminarPlatillo(int id)
+        {
+            _token = Session["Token"].ToString();
+            var plat = new Platillo() { Token = _token };
+            plat.EliminarPlatillo(id);
+            return RedirectToAction("VerPlatillos");
         }
     }
 }
