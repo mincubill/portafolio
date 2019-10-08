e8dfd705a28ea2558a9470de0dfd1f500130b23f
andres cubillos <mincubill@gmail.com>
2019-10-08 20:11:52 -0300

    merge a mano por safdhashdjkshadjka vez

--- Cliente/SigloXXI/SigloXXI.Data/Mesas.cs
+++ Cliente/SigloXXI/SigloXXI.Data/Mesas.cs
@@ -11,40 +11,54 @@ namespace SigloXXI.Data
         public int Id { get; set; }
         public int Numero { get; set; }
         public int Capacidad { get; set; }
+        public string Token { get; set; }
+
+        public bool CrearMesa(Mesas mesa)
+        {
+            
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", mesa.Id.ToString() },
+                { "numero",  mesa.Numero.ToString() },
+                {"capacidad",  mesa.Capacidad.ToString() },
+            };
+            JsonHelper<Mesas>.Token = this.Token;
+            return JsonHelper<Mesas>.Post(queryParams, "/mesas/crear-mesa");
+        }
+
+        public bool ActualizarMesa(Mesas mesa)
+        {
+           
+            var queryParams = new Dictionary<string, string>
+            {
+                {"id", mesa.Id.ToString() },
+                { "numero",  mesa.Numero.ToString() },
+                {"capacidad",  mesa.Capacidad.ToString() },
+            };
+            JsonHelper<Mesas>.Token = this.Token;
+            return JsonHelper<Mesas>.Put(queryParams, "/mesas/actualizar-mesa/" + mesa.Id);
+        }
 
         public List<Mesas> ObtenerMesas()
         {
+            JsonHelper<Mesas>.Token = this.Token;
             var result = JsonHelper<Mesas>.GetList("/mesas/obtener-mesas");
             return result;
         }
 
         public Mesas ObtenerMesa(int id)
         {
-
-            return new Mesas();
+            JsonHelper<Mesas>.Token = this.Token;
+            var queryParams = new Dictionary<string, string>();
+            var res = JsonHelper<Mesas>.Get(queryParams, "/mesas/buscar-mesa/" + id.ToString());
+            return res;
         }
 
-        public bool CrearMesas(Mesas mesa)
+        public bool EliminarMesa(int id)
         {
-            var queryParams = new Dictionary<string, string>
-            {
-                {"id", mesa.Id.ToString() },
-                {"numero",  mesa.Numero.ToString()},
-                {"capacidad",  mesa.Capacidad.ToString()},
-            };
-            return JsonHelper<Mesas>.Post(queryParams, "/mesas/crear-mesa");
-        }
-
-        public bool EditarMesa(Mesas mesas)
-        {
-            var id = mesas.Id;
-            return false;
-        }
-
-        public bool EliminarMesa(int Id)
-        {
-            var id = Id;
-            return false;
+            JsonHelper<Mesas>.Token = this.Token;
+            var queryParams = new Dictionary<string, string>();
+            return JsonHelper<Mesas>.Delete(queryParams, "/mesas/eliminar-mesa/" + id.ToString());
         }
     }
 }
--- Cliente/SigloXXI/SigloXXI/Controllers/MesasController.cs
+++ Cliente/SigloXXI/SigloXXI/Controllers/MesasController.cs
@@ -2,72 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using SigloXXI.Data;
 using SigloXXI.Models;
+using System.Web.Mvc;
+
 
 namespace SigloXXI.Controllers
 {
-    public class MesasController : Controller
+    public class MesaController : Controller
     {
-        // GET: Mesas
-        public ActionResult VerMesasDisponibles()
+        string _token;
+        public ActionResult VerMesas()
         {
-            var mesa = new Mesas();
-            ViewData["mesas"] = mesa.ObtenerMesas();
-            return View();
-        }
-
-        public ActionResult AgregarMesa()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AgregarMesa(MesaModel model)
-        {
-            var mesa = new Mesas()
-            {
-                Id = model.Id,
-                Capacidad = model.Capacidad,
-                Numero = model.Numero,
-            };
-            mesa.CrearMesas(mesa);
+            _token = Session["Token"].ToString();
+            var mesa = new Mesas() { Token = _token };
+            ViewData["Mesas"] = mesa.ObtenerMesas();
             return View();
         }
 
         [HttpGet]
-        public ActionResult EditarMesa(int id)
+        public ActionResult AgregarMesas()
         {
-            var mesa = new Mesas();
-            mesa.ObtenerMesa(id);
+            _token = Session["Token"].ToString();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarMesas(MesaModel model)
+        {
+            _token = Session["Token"].ToString();
+            var mesa = new Mesas()
+            {
+                Token = _token,
+                Id = model.Id,
+                Numero = model.Numero,
+                Capacidad = model.Capacidad,
+            };
+            mesa.CrearMesa(mesa);
+            return RedirectToAction("VerMesas");
+        }
+
+        [HttpGet]
+        public ActionResult EditarMesas(int id)
+        {
+            _token = Session["Token"].ToString();
+            var mesa = new Mesas() { Token = _token };
+            mesa = mesa.ObtenerMesa(id);
             MesaModel model = new MesaModel
             {
-                Capacidad = mesa.Capacidad,
                 Id = mesa.Id,
                 Numero = mesa.Numero,
+                Capacidad = mesa.Capacidad,
             };
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult EditarMesa(MesaModel model)
+        public ActionResult EditarMesas(MesaModel model)
         {
+            _token = Session["Token"].ToString();
             var mesa = new Mesas()
             {
+                Token = _token,
                 Id = model.Id,
-                Capacidad = model.Capacidad,
                 Numero = model.Numero,
+                Capacidad = model.Capacidad,
             };
-            mesa.EditarMesa(mesa);
-            return RedirectToAction("VerMesasDisponibles");
+            mesa.ActualizarMesa(mesa);
+            return RedirectToAction("VerMesas");
         }
 
-        public ActionResult EliminarMesa(int id)
+        public ActionResult EliminarMesas(int id)
         {
-            var mesa = new Mesas();
+            _token = Session["Token"].ToString();
+            var mesa = new Mesas() { Token = _token };
             mesa.EliminarMesa(id);
-            return RedirectToAction("VerMesasDisponibles");
+            return RedirectToAction("VerMesas");
         }
     }
 }
